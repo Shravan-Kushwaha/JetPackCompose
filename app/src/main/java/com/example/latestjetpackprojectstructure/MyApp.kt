3990c5e26372b826127e9cf0278ad2ecf70db7de
shravanEWW <shravan.kushwaha@excellentwebworld.in>
2024-02-13 12:22:13 +0530

    commnet added to test cicd

--- app/src/main/java/com/example/latestjetpackprojectstructure/MyApp.kt
+++ app/src/main/java/com/example/latestjetpackprojectstructure/MyApp.kt
@@ -3,7 +3,7 @@ package com.example.latestjetpackprojectstructure
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
-
+/*Application File*/
 @HiltAndroidApp
 class MyApp: Application() {
 }
